d6856c05cda3af980812881797535e7fc82f0cad
ShenJiaquan <1374169022@qq.com>
2019-10-25 10:55:09 +0800

    把springboot-batis-plus加入到git
    Signed-off-by: ShenJiaquan <1374169022@qq.com>

--- springbootmybatisplus/src/main/java/com/jh/springbootmybatisplus/SpringbootmybatisplusApplication.java
+++ springbootmybatisplus/src/main/java/com/jh/springbootmybatisplus/SpringbootmybatisplusApplication.java
@@ -1,17 +1,11 @@
 package com.jh.springbootmybatisplus;
 
-import com.google.gson.Gson;
-import com.jh.springbootmybatisplus.mapper.UserMapper;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.logging.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class SpringbootmybatisplusApplication {
 
     public static void main(String[] args) throws Exception {
